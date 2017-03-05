50f08312b9cfc283c50dc43e45cdfd915035e54a
Megan Aldridge <m.aldridge@live.com>
2017-03-05 01:00:47 -0800

    smallsh.c

--- smallsh.c
+++ smallsh.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 
 /* Global */
 pid_t parent_PID;
@@ -19,23 +20,29 @@ char statusMsg[30];
 pid_t background_PIDS[400];
 
 
+void catchSIGINT(int signo) {
+	char * message = "Caught SIGINT, sleeping for 5 seconds\n";
+	write(STDOUT_FILENO, message, 38);
+	raise(SIGUSR2);
+	sleep(5);
+}
 
+/*void catchSIGUSR2(int signo) {
+	char * message = "Caught SIGUSR2, exiting!\n";
+	write(STDOUT_FILENO, message, 25);
+	exit(0);
+
+}*/
 
 int isValidInput() {
 	if ( first == '#') {
-//		printf("you entered a # sign\n");
-//		fflush(stdout);
 		return 0;
 	}
 
 	else if (first == '\n') {
-//		printf("you entered a blank line\n");
-//		fflush(stdout);
 		return 0;
 	}
 	else if (first == '\t' && numArgs == 1) {
-//		printf("you entered a tab\n");
-//		fflush(stdout);
 		return 0;
 	} 
 
@@ -44,8 +51,8 @@ int isValidInput() {
 }
 
 void cleanup() {
-	printf("now exiting\n");
-	fflush(stdout);
+
+	kill(0, SIGKILL);
 }
 
 void callDirectory() {
@@ -57,18 +64,8 @@ void callDirectory() {
 		exit;
 	}
 	
-	printf("CWD: %s\n", cwd);
-	fflush(stdout);
-
-//	printf("first two characters are cd\n");
-//	fflush(stdout);	
 
 	if (strcmp(message, "cd") == 0) {
-//		printf("Only characters are cd\n");
-//		fflush(stdout);
-
-		printf("Home environment: %s\n", getenv("HOME"));
-		fflush(stdout);
 
 		chdir(getenv("HOME"));
 	
@@ -77,22 +74,17 @@ void callDirectory() {
 			exit;
 		}	
 	
-		printf("CWD: %s\n", cwd);
-		fflush(stdout); 
 	}
 	else {
-//		printf("arg1: %s\n", args[1]);
-//		fflush(stdout);
 
 		chdir(args[1]);
 	}	
 }
 
-void status(pid_t fgPid) {
+void status() {
+	
 	printf(statusMsg);
 	fflush(stdout);
-
-
 
 }
 
@@ -107,12 +99,6 @@ void getArgs() {
 	/* Copy message to temp variable to get arguments */
 	strcpy(temp, message);
 
-	printf("Message: %s\n", message);
-	fflush(stdout);
- 
-//	printf("temp: %s\n", temp);
-//	fflush(stdout);
-
 	/* Get number of arguments and assign to arg array */
 	for (i = 0; i < 512; i++) {
 		args[i] = malloc(30);
@@ -123,11 +109,9 @@ void getArgs() {
 	token = strtok(temp, " ");
 
 	while (	token != NULL) {
-		//args[i] = token;
+	
 		strcpy(args[i], token);
 		i++;
-	//	printf("token[%d]=%s, args=%s", i-1, token, args[i-1]);
-	//	fflush(stdout);
 
 	token = strtok(NULL, " ");
 		
@@ -142,36 +126,24 @@ void getArgs() {
 	/* Get first character of last argument */
 	last = *args[numArgs-1];
 
-
-
 }
 
 void appendPID() {
 
 	int j,k,l;
-//	printf("arg[0] length is: %d\n", strlen(args[1]));
+
 	for (j=0; j < numArgs; j++) {
 	
 		l = strlen(args[j])-1;
 		for (k=0; k<l; k++) {
 			if (*(args[j]+k) == *(args[j]+k+1)) {
 				if (*(args[j]+k) == '$') {
-//					printf("arg: %s\n", args[j]);
-//					fflush(stdout);
-				
-//					printf("argj + k = %s", args[j]+k);
-					fflush(stdout);
 
 					sprintf(args[j]+k, "%d",parent_PID);
-
-//					printf("arg: %s\n",args[j]);
-//					fflush(stdout);
 
 				}
 				
 			}
-//				printf("%d %d = %c%c\n ", k,k+1,*(args[j]+k),*(args[j]+k+1));
-//				fflush(stdout);
 		}
 
 
@@ -269,12 +241,7 @@ void redirectAndExecute(int foreground) {
 	char * params[10];
 	int numParams = 0;	
 	
-	printf("redirectAndExecute\n");
-	fflush(stdout);
-
 	if (!foreground) {
-		printf("bg process remove & arg\n");
-		fflush(stdout);
 
 		/* Prevent output and input from being read from terminal */
 		devNull = open("/dev/null", O_RDWR);
@@ -293,62 +260,37 @@ void redirectAndExecute(int foreground) {
 	
 	for (i=0; i < numArgs; i++) {
 		if ( strcmp(args[i], ">") == 0 ) {
-			printf("args[%d] is >\n", i);
-			fflush(stdout);
 	
 			strcpy(outFile, args[i + 1]);
-
-			printf("output file is: %s\n", outFile);
-			fflush(stdout);
 
 			outFD = open(outFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 			if (outFD == -1) {
 				printf("cannot open %s for output\n", outFile);
 				fflush(stdout);
-				statusVal = 1;	
 				exit(1);	
 			}	
 			
 			args[i] = NULL;
-//			args[i+1] = " ";		
 		}
 		else if ( strcmp(args[i],"<") == 0 ) {
-			printf("args[%d] is <\n", i);
-			fflush(stdout);
 
 			strcpy(inFile, args[i + 1]);
-
-			printf("input file is: %s\n", inFile);
-			fflush(stdout); 
 
 			inFD = open(inFile, O_RDONLY);
 			
 			if (inFD == -1) {
 				printf("cannot open %s for input\n", inFile);
 				fflush(stdout);
-				statusVal = 1;
 				exit(1);
 			}
 			args[i] = NULL;
-//			args[i+1] = "";
 
 		}
 
-		else {
-
-		//	printf("else!\n");
-		//	printf("numParams = %d and i = %d\n", numParams, i);
-		//	params[numParams] =  args[i];
-		//	printf("params[%d] = %s\n", numParams, params[numParams]);
-		//	fflush(stdout);
-		//	numParams++;		
-	
-		}
 
 	}
 
-	displayArgs();
 
 	if (outFD != -5) {
 		result = dup2(outFD, 1);
@@ -375,20 +317,7 @@ void redirectAndExecute(int foreground) {
 	
 	}
 
-/*	for (i = 1; i < numArgs; i++) {
 			
-		if (strcmp(args[i], "<")) {
-			args[i] = NULL;
-		}
-		else {
-	
-
-	}
-*/
-
-//	printf("exec time\n");
-//	fflush(stdout);
-
 	if ( execvp(args[0],args) < 0 ) {
 		perror(args[0]);
 		exit(1);
@@ -410,9 +339,26 @@ int main( int argc, char * argv[]) {
 	pid_t child_PID;
 	int result;
 
-	/* Print shell PID */
-	printf("Shell PID: %i\n", parent_PID);
-	fflush(stdout);
+	/*Initialize Signal Handlers */
+	struct sigaction SIGINT_action = {0};
+//	SIGINT_action.sa_handler = catchSIGINT;
+	SIGINT_action.sa_handler = SIG_IGN;
+	sigfillset(&SIGINT_action.sa_mask);
+	SIGINT_action.sa_flags = 0;
+
+	/* Signal actions in shell */
+	sigaction(SIGINT, &SIGINT_action, NULL);
+
+
+//	sigfillset(&SIGUSR2_action.sa_mask);
+//	SIGUSR2_action.sa_flags = 0;
+//	sigaction(SIGUSR2, &SIGUSR2_action, NULL);
+//	sigaction(SIGUSR2, &ignore_action, NULL);
+//	sigaction(SIGTERM, &ignore_action, NULL);
+//	sigaction(SIGHUP, &ignore_action, NULL);
+//	sigaction(SIGQUIT, &ignore_action, NULL);
+
+	
 
 	/* Initialize array of PIDs to zero */
 	for (i =0; i < 400; i++) {
@@ -425,12 +371,11 @@ int main( int argc, char * argv[]) {
 	while(1) {
 
 	/* Display list of background pids for debugging */
-	displayPIDS();
+//	displayPIDS();
 
 	/* Check BG pids array for ended processes */
 	checkPIDS();
 
-	/* Display list of background pids for debugging */
 
 	foreground = 1;
 	i = 0;
@@ -460,19 +405,11 @@ int main( int argc, char * argv[]) {
 	/* Look through arguments and replace $$ with shell pid */
 	appendPID();
 
-	/* display args for debugging */
-	displayArgs();
 
-//	printf("first char: %c\n", first);
-
-	if (last == '&') {
-//		printf("last char: %c\n", last);
+	if ((last == '&') && (strcmp(args[0], "echo") != 0) ) {
 		foreground = 0;
 	}
 
-//	printf("message: %s\n", message);
-//	fflush(stdout);
-	
 
 	/* If cd command received, change working directory */
 	if (strstr(message, "cd") == message) {
@@ -493,29 +430,20 @@ int main( int argc, char * argv[]) {
 			exit(1);
 	}
 	else if (spawnPid == 0) {
-			printf("I am the child process in action\n");
-			fflush(stdout);
-//			printf("My parent: %i Me: %i\n", getppid(), getpid());
-			fflush(stdout);
+
+			SIGINT_action.sa_handler = SIG_DFL;
+			sigaction(SIGINT, &SIGINT_action, NULL);
 
 			redirectAndExecute(foreground);
 
-			sleep(20);
-			
-			printf("done sleeping\n");
-			fflush(stdout);
 			exit(0);
 	}
 
 	
 	if (foreground) {
 		int exitStatus, termSignal;
-//		printf("Foreground process!\n");
-//		fflush(stdout);
 	
 		memset(statusMsg, '\0', sizeof(statusMsg));
-		printf("PARENT: PID: %d, waiting...\n", spawnPid);
-		fflush(stdout);
 
 		result = waitpid(spawnPid, &childExitMethod, 0);
 
@@ -526,19 +454,18 @@ int main( int argc, char * argv[]) {
 		if (WIFEXITED(childExitMethod)) {
 			exitStatus = WEXITSTATUS(childExitMethod);
 			snprintf(statusMsg, sizeof(statusMsg), "exit value %d\n", exitStatus);
-		//	fflush(stdout);
 		}			
 
 		else {
 			termSignal = WTERMSIG(childExitMethod);	
 			snprintf(statusMsg, sizeof(statusMsg), "terminated by signal %d\n", termSignal);
-		//	fflush(stdout);				
+			if (termSignal == 2) {
+				printf(statusMsg);
+			}
 		}
 	
 
 
-		printf("PARENT: Child process terminated, exiting!\n");
-		fflush(stdout);	
 	}
 	else {
 		printf("background pid is %d\n", spawnPid);
